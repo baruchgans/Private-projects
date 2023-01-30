3cf798beda30797deb28b1ab24098194298808db
Baruch Gans <70571819+baruchgans@users.noreply.github.com>
2023-01-30 22:13:26 +0200

    implemented using django

--- pokedex_assignment/pokedex/pokedex/settings.py
+++ pokedex_assignment/pokedex/pokedex/settings.py
@@ -114,11 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -128,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
--- pokedex_assignment/pokedex/pokedex_app/views.py
+++ pokedex_assignment/pokedex/pokedex_app/views.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+from django.core.paginator import PageNotAnInteger, EmptyPage
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
@@ -5,16 +7,44 @@ from . import db
 
 
 def pokedex(request, page_number):
-    page_size = request.GET.get('page_size', '10')
-    # Get all pokemons from the DB
-    pokemons = db.get()
+    page_size = int(request.GET.get('page_size', '10'))
+    sort_direction = request.GET.get('sort_direction', 'asc')
+    filter_term = request.GET.get('filter_term', '')
 
-    # Create a paginator object with a page size of 10
+    # Try to get the pokemons from cache
+    cache_key = f"pokemons_{page_size}_{sort_direction}_{filter_term}"
+    pokemons = cache.get(cache_key)
+
+    # If the pokemons are not in cache, get them from the database
+    if pokemons is None:
+        # Get all pokemons from the DB
+        pokemons = db.get()
+
+        # Filter the pokemons based on the filter term
+        if filter_term:
+            pokemons = [pokemon for pokemon in pokemons if filter_term in pokemon['type_one'].lower()]
+
+        if sort_direction == 'desc':
+            pokemons = sorted(pokemons, key=lambda x: x['number'], reverse=True)
+        else:
+            pokemons = sorted(pokemons, key=lambda x: x['number'])
+
+        # Store the pokemons in cache for 5 minutes
+        cache.set(cache_key, pokemons, 300)
+
+    # Create a paginator object with the selected page size
     paginator = Paginator(pokemons, page_size)
 
-    # Get the current page of pokemons
-    page_pokemons = paginator.get_page(page_number)
+    try:
+        # Get the current page of pokemons
+        page_pokemons = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        page_pokemons = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        page_pokemons = paginator.page(paginator.num_pages)
 
-    # Render the pokedex template with the current page of pokemons
-    return render(request, 'pokedex.html', {'pokemons': page_pokemons, 'page_size': page_size})
-
+    return render(request, 'pokedex.html',
+                  {'pokemons': page_pokemons, 'page_size': page_size, 'filter_term': filter_term,
+                   'sort_direction': sort_direction})
