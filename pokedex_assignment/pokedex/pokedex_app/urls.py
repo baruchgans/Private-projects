d8a230a796270341f55c6fc3e27e44702960b83b
Baruch Gans <70571819+baruchgans@users.noreply.github.com>
2023-01-31 13:46:46 +0200

    add pokedex_assignment/requirements.txt update urls and add default implement "Assume the DB is live and changes can be made to it during the server lifetime."

--- pokedex_assignment/pokedex/pokedex_app/urls.py
+++ pokedex_assignment/pokedex/pokedex_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.pokedex, name='pokedex_default'),
     path('page/<int:page_number>/', views.pokedex, name='pokedex'),
 
 ]
--- pokedex_assignment/pokedex/pokedex_app/views.py
+++ pokedex_assignment/pokedex/pokedex_app/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.core.cache import cache
 from django.core.paginator import PageNotAnInteger, EmptyPage
 from django.core.paginator import Paginator
@@ -5,8 +7,10 @@ from django.shortcuts import render
 
 from . import db
 
+TTL_TIME = 300
 
-def pokedex(request, page_number):
+
+def pokedex(request, page_number=1):
     page_size = int(request.GET.get('page_size', '10'))
     sort_direction = request.GET.get('sort_direction', 'asc')
     filter_term = request.GET.get('filter_term', '')
@@ -15,8 +19,9 @@ def pokedex(request, page_number):
     cache_key = f"pokemons_{page_size}_{sort_direction}_{filter_term}"
     pokemons = cache.get(cache_key)
 
-    # If the pokemons are not in cache, get them from the database
-    if pokemons is None:
+    # If the pokemons are not in cache, or if the database has changed since the last cache refresh,
+    # get them from the database
+    if pokemons is None or has_changed():
         # Get all pokemons from the DB
         pokemons = db.get()
 
@@ -30,7 +35,11 @@ def pokedex(request, page_number):
             pokemons = sorted(pokemons, key=lambda x: x['number'])
 
         # Store the pokemons in cache for 5 minutes
-        cache.set(cache_key, pokemons, 300)
+        cache.set(cache_key, pokemons, TTL_TIME)
+        cache.set("timestamp", time.time(), TTL_TIME)
+
+        # TODO: Implement cache tags to invalidate cache only for changed records
+        # Example: cache.set_many({cache_key: pokemons}, TTL_TIME, ('pokemon',))
 
     # Create a paginator object with the selected page size
     paginator = Paginator(pokemons, page_size)
@@ -48,3 +57,21 @@ def pokedex(request, page_number):
     return render(request, 'pokedex.html',
                   {'pokemons': page_pokemons, 'page_size': page_size, 'filter_term': filter_term,
                    'sort_direction': sort_direction})
+
+
+def has_changed():
+    # Get the timestamp of the last cache update
+    last_update = cache.get("timestamp")
+
+    # If there was no previous update, return True
+    if last_update is None:
+        return True
+
+    # Get the current time
+    now = time.time()
+
+    # If the time since the last update is greater than the cache time-to-live (TTL), return True
+    if now - last_update > TTL_TIME:
+        return True
+
+    return False
